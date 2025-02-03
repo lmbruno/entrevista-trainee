837268b9cdfdcfc4fe1a0b8b8bc04c9829fc38a0
lmbruno <bmlopez93@gmail.com>
2025-02-03 19:29:59 -0300

    adding first e2e test

---
+++ tests/catFactApp.spec.js
@@ -0,0 +1,23 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+
+const LOCALHOST = "http://localhost:5173/";
+
+test("has title", async ({ page }) => {
+  await page.goto(LOCALHOST);
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/CatFact App/);
+});
+
+// test("get started link", async ({ page }) => {
+//   await page.goto("https://playwright.dev/");
+
+//   // Click the get started link.
+//   await page.getByRole("link", { name: "Get started" }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(
+//     page.getByRole("heading", { name: "Installation" })
+//   ).toBeVisible();
+// });
