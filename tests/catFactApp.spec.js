0ac18a88322efcfb84c18c2c4afbf5cacc312ab3
lmbruno <bmlopez93@gmail.com>
2025-02-04 15:35:52 -0300

    adding more tests

--- tests/catFactApp.spec.js
+++ tests/catFactApp.spec.js
@@ -3,21 +3,43 @@ import { test, expect } from "@playwright/test";
 
 const LOCALHOST = "http://localhost:5173/";
 
-test("has title", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto(LOCALHOST);
+});
 
-  // Expect a title "to contain" a substring.
+test("has title", async ({ page }) => {
   await expect(page).toHaveTitle(/CatFact App/);
 });
 
-// test("get started link", async ({ page }) => {
-//   await page.goto("https://playwright.dev/");
+test("app is showing cat fact", async ({ page }) => {
+  const catFact = await page.getByRole("paragraph").innerText;
 
-//   // Click the get started link.
-//   await page.getByRole("link", { name: "Get started" }).click();
+  await expect(catFact.length).toBeGreaterThan(0);
+});
 
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(
-//     page.getByRole("heading", { name: "Installation" })
-//   ).toBeVisible();
-// });
+test("app is showing cat image", async ({ page }) => {
+  const img = await page.getByRole("img");
+  const imgSrc = await img.getAttribute("src");
+
+  await expect(imgSrc?.length).toBeGreaterThan(0);
+});
+
+test("fact is changing when button is clicked", async ({ page }) => {
+  const paragraph = await page.getByRole("paragraph");
+  const catFact = await paragraph.innerText();
+
+  await page.getByRole("button").click();
+
+  await expect(page.getByRole("paragraph")).not.toHaveText(catFact);
+  await expect(page.getByRole("paragraph")).toBeVisible();
+});
+
+test("image is changing when button is clicked", async ({ page }) => {
+  const img = await page.getByRole("img");
+  const imgSrc = await img.getAttribute("src");
+
+  await page.getByRole("button").click();
+
+  await expect(page.getByRole("img")).toBeVisible();
+  await expect(page.getByRole("img").getAttribute("src")).toBeDefined();
+});
